0a8d7f60f36733dccdba5383f3d3b37f8d5d470e
koreafirstec1988@gmail.com <koreafirstec>
2021-03-09 19:10:31 +0900

    [임혁진] Ipad swipe 테스트

--- src/server/titan_sever/ai/models/plots.py
+++ src/server/titan_sever/ai/models/plots.py
@@ -19,8 +19,8 @@ from PIL import Image, ImageDraw
 from scipy.signal import butter, filtfilt
 from sklearn.cluster import KMeans
 
-from utils.general import xywh2xyxy, xyxy2xywh
-from utils.metrics import fitness
+from .general import xywh2xyxy, xyxy2xywh
+from .metrics import fitness
 
 # Settings
 matplotlib.rc('font', **{'size': 11})
